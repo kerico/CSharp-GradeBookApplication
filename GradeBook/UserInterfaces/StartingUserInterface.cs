cc6274464a893fdd973f5bae072283095502f332
Augustas <augjec@gmail.com>
2018-06-13 20:33:42 +0300

    added 'Type' variable to create command
    Create now creates grade book of name 'Name' and of type 'Type'. Help command updated to show the new addition of 'Type'

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -39,8 +39,22 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
+            var type = parts[2];
+            BaseGradeBook gradeBook;
+            if (type.ToLower().Equals("standard"))
+            {
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if (type.ToLower().Equals("ranked"))
+            {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
+                return;
+            }
+            Console.WriteLine("Created gradebook {0} of type {1}.", name,type);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
@@ -65,7 +79,7 @@ namespace GradeBook.UserInterfaces
         {
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
